1670c50f67170ae0b20656a1aae00541465e21e8
Jared-Wasson <jd.wasson2@gmail.com>
2022-12-16 17:19:38 -0700

    cleanup

--- pi.service.js
+++ pi.service.js
@@ -6,20 +6,7 @@ var port = 3000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-function makeArray(w, h, val) {
-    var arr = [];
-    for (var i = 0; i < h; i++) {
-        arr[i] = [];
-        for (var j = 0; j < w; j++) {
-            arr[i][j] = val;
-        }
-    }
-    return arr;
-}
-var grid = makeArray(5, 5, 0);
-var robots = [];
 var objectDetected = false;
-var lastPosition = {};
 app.get('/objectDetected', function (req, res) {
     res.send(objectDetected);
 });
@@ -27,9 +14,4 @@ app.post('/foundObject', function (req, res) {
     objectDetected = true;
     res.send(objectDetected);
 });
-app.get('/getNextPosition/:id', function (req, res) {
-    var id = req.params.id;
-    var currentPosition = req.params.position;
-    res.send();
-});
-app.listen(port, function () { return console.log("Hello world app listening on port ".concat(port, "!")); });
+app.listen(port, function () { return console.log("listening on port ".concat(port, "!")); });
--- pi.service.ts
+++ pi.service.ts
@@ -8,21 +8,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-function makeArray(w: number, h: number, val: number): number[][] {
-    var arr: number[][] = [];
-    for(let i = 0; i < h; i++) {
-        arr[i] = [];
-        for(let j = 0; j < w; j++) {
-            arr[i][j] = val;
-        }
-    }
-    return arr;
-}
-
-const grid = makeArray(5,5,0);
-const robots: number[] = [];
 let objectDetected = false;
-let lastPosition = {};
 
 app.get('/objectDetected', (req, res) => {
     res.send(objectDetected);
@@ -33,11 +19,4 @@ app.post('/foundObject', (req, res) => {
     res.send(objectDetected);
 });
 
-app.get('/getNextPosition/:id', (req, res) => {
-    const id = req.params.id;
-    const currentPosition = req.params.position;
-    res.send();
-
-})
-
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
+app.listen(port, () => console.log(`listening on port ${port}!`))
